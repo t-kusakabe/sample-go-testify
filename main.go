82100320179a44b946e4dc6f4c4c9c8973fda1c7
t-kusakabe <ttmac0704@gmail.com>
2019-10-09 19:53:35 +0900

    Sample test

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Calculate is return int
+func Calculate(x int) (result int) {
+	result = x + 2
+	return result
+}
+
+func main() {
+	fmt.Println("Hello World")
+}
